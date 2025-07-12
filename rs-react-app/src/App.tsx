6662c11a91e3001cf42ec76c45348f17e2e63157
Kathering Rivera <katheriverar@gmail.com>
2025-07-12 08:36:22 -0500

    feat: add loading spinner

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import SearchBar from './components/SearchBar';
 import CardList from './components/CardList';
+import Spinner from './components/Spinner';
 import {
   fetchPokemonByName,
   fetchPokemonList,
@@ -37,7 +38,7 @@ class App extends Component<{}, AppState> {
     this.setState({ loading: true, error: null });
 
     try {
-      const list = await fetchPokemonList(0, 5); // carga 5 por defecto
+      const list = await fetchPokemonList(0, 12);
       const detailed = await Promise.all(
         list.map((p) => fetchPokemonByName(p.name))
       );
@@ -75,7 +76,7 @@ class App extends Component<{}, AppState> {
         <h1 className="text-3xl font-bold mb-4">🔍 Pokémon Search</h1>
         <SearchBar onSearch={this.handleSearch} />
 
-        {loading && <p className="mt-4">Buscando...</p>}
+        {loading && <Spinner />}
 
         {error && <p className="mt-4 text-red-600 font-semibold">{error}</p>}
 
@@ -86,7 +87,7 @@ class App extends Component<{}, AppState> {
           onClick={() => {
             throw new Error('Prueba de error');
           }}
-          className=""
+          className="mt-4 px-4 py-2 !bg-red-500 text-white rounded hover:bg-red-600 hover:!outline-red-400 hover:!border-red-400"
         >
           Lanzar error
         </button>
