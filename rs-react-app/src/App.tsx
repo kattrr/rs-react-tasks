847256e6e6c5332b717e5abf5e195fa00ce778c8
Kathering Rivera <katheriverar@gmail.com>
2025-07-12 08:03:56 -0500

    feat: test api connection with temporary console log and sample UI

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import  { Component } from 'react';
+import { fetchPokemonList, fetchPokemonByName } from './api/pokeapi';
 
-function App() {
-  const [count, setCount] = useState(0)
+class App extends Component {
+  componentDidMount(): void {
+    fetchPokemonList(0, 10)
+      .then((data) => {
+        console.log('Lista de Pokémon:', data);
+      })
+      .catch((err) => {
+        console.error('Error en lista:', err);
+      });
 
-  return (
-    <>
+    fetchPokemonByName('pikachu')
+      .then((data) => {
+        console.log('Datos de Pikachu:', data);
+      })
+      .catch((err) => {
+        console.error('Error al buscar Pikachu:', err);
+      });
+  }
+
+  render() {
+    return (
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>PokeAPI Test</h1>
+        <p>Abre la consola del navegador (F12) para ver los resultados</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
+  }
 }
 
-export default App
+export default App;
