2f4a0d653bbf4402dfd4fbd1070fb14457851421
Kathering Rivera <katheriverar@gmail.com>
2025-07-12 08:36:12 -0500

    style: add card and cardList style

--- rs-react-app/src/components/Card.tsx
+++ rs-react-app/src/components/Card.tsx
@@ -10,16 +10,14 @@ class Card extends Component<CardProps> {
     const { name, sprites, types } = this.props.pokemon;
 
     return (
-      <div className="bg-white">
+      <div className="bg-white rounded-4xl flex flex-col items-center p-4 shadow-md">
         <img
           src={sprites.front_default}
           alt={name}
           className=""
         />
-        <h2 className="">{name}</h2>
-        <p className="">
-          {types.map((t) => t.type.name).join(', ')}
-        </p>
+        <h2 className="text-black">name: {name}</h2>
+        <p className="text-black">type: {types.map((t) => t.type.name).join(', ')}</p>
       </div>
     );
   }
--- rs-react-app/src/components/CardList.tsx
+++ rs-react-app/src/components/CardList.tsx
@@ -11,7 +11,7 @@ class CardList extends Component<CardListProps> {
     const { pokemons } = this.props;
 
     return (
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 mt-6">
         {pokemons.map((pokemon) => (
           <Card key={pokemon.name} pokemon={pokemon} />
         ))}
