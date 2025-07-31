02fb8e7600356d49772689acdecd8e2375f6fbec
Kathering Rivera <katheriverar@gmail.com>
2025-07-31 15:22:33 -0500

    refactor: update component exports to include new components

--- rs-react-app/src/components/index.ts
+++ rs-react-app/src/components/index.ts
@@ -3,3 +3,5 @@ export { default as CardList } from './CardList';
 export { default as ErrorBoundary } from './ErrorBoundary';
 export { default as SearchBar } from './SearchBar';
 export { default as Spinner } from './Spinner';
+export { default as SelectedItemsFlyout } from './SelectedItemsFlyout';
+export { default as ThemeSelector } from './ThemeSelector';
