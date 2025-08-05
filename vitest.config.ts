0e8177eb55d4e2c5eb691909ddc3971709da7fa2
Kathering Rivera <katheriverar@gmail.com>
2025-08-05 15:44:04 -0500

    fix: config paths

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@api': path.resolve(__dirname, './src/api'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@contexts': path.resolve(__dirname, './src/contexts'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@test': path.resolve(__dirname, './src/_test_'),
+    },
+  },
   test: {
     environment: 'jsdom',
     globals: true,
