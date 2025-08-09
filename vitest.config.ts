400db49862d5633cde5ad11b5d2dfc75f9f70472
Kathering Rivera <katheriverar@gmail.com>
2025-08-08 22:10:55 -0500

    fix: building imports path

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
       '@api': path.resolve(__dirname, './src/api'),
-      '@components': path.resolve(__dirname, './src/components'),
+      '@components': path.resolve(__dirname, 'src/components'),
       '@contexts': path.resolve(__dirname, './src/contexts'),
       '@hooks': path.resolve(__dirname, './src/hooks'),
       '@pages': path.resolve(__dirname, './src/pages'),
