bc061c0ba7008ea7553d41a539c2d2cad66c0769
Kathering Rivera <katheriverar@gmail.com>
2025-08-14 16:23:29 -0500

    feat: add Next.js configuration

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  distDir: './dist', // Changes the build output directory to `./dist/`
+  trailingSlash: true,
+  images: {
+    unoptimized: true, // Required for static export
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname: '/PokeAPI/sprites/**',
+      },
+    ],
+  },
+};
+
+export default withNextIntl(nextConfig);
